14bc743db28a3c7f895a86cf463430a6818f7bd4
buingochieuc7 <99152917+buingochieuc7@users.noreply.github.com>
2022-06-14 07:50:30 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from __future__ import print_function
 import subprocess
 import os
 print("[*] Installing requirements.txt...")
-subprocess.Popen("pip3 install -r requirements.txt", shell=True).wait()
+subprocess.Popen("pip install -r requirements.txt", shell=True).wait()
 print("[*] Installing setoolkit to /usr/share/setoolkit..")
 print(os.getcwd())
 subprocess.Popen("mkdir /usr/share/setoolkit/;mkdir /etc/setoolkit/;cp -rf * /usr/share/setoolkit/;cp src/core/config.baseline /etc/setoolkit/set.config", shell=True).wait()
